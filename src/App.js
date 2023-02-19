4c503bd1cd3a1838628240566ce6ba8bd132def4
AnastasiaPanysh <nastya.panysh@gmail.com>
2023-02-19 22:30:00 +0300

    add all

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import Page from "./pages/Page";
+import style from "./App.module.css"
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
-function App() {
+function Page() {
+  const [ip, setIP] = useState()
+
+  async function request() {
+    const response = await axios.get('https://api.ipify.org/?format=json')
+    setIP(response.data.ip)
+  }
+
+  useEffect(() => {
+    request()
+  })
+
   return (
-    <>
-      <Page/>
-    </>
-  );
+    <div>
+      <h1>{ip ? ip : '...'}</h1>
+      <p>  This is your {ip}IP address probably :P </p>
+    </div>
+  )
+
+
 }
 
-export default App;
+export default Page;
